5d93af115995424b851b24cf96070ce309a999a2
eidersin <dersonts@gmail.com>
2023-12-29 13:19:28 -0300

    Update ex0017-calculoHipotenusa.py

--- PYTHON/Exercicios/ex0017-calculoHipotenusa.py
+++ PYTHON/Exercicios/ex0017-calculoHipotenusa.py
@@ -3,7 +3,7 @@ from math import hypot
 co = float(input('Entre com o valor do cateto oposto: '))
 ca = float(input('Entre com o valor do cateto adjacente: '))
 
-hipot = (co ** 2 + ca ** 2) ** (1 / 2)
+# hipot = (co ** 2 + ca ** 2) ** (1 / 2)
 
-print('A hipotenusa vai medir {:.2f}'.format(hipot))
+# print('A hipotenusa vai medir {:.2f}'.format(hipot))
 print('A hipotenusa vai medir {:.2f}'.format(hypot(co, ca)))
