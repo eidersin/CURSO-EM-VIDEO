aaaa8f593cbc445848a34909b12dbbfec91c647c
eidersin <dersonts@gmail.com>
2024-01-30 17:55:04 -0300

    feat: update code ex 65

--- PYTHON/MUNDO 02/Exercicios/Ex65_media_maior_menor_varios_numeros.py
+++ PYTHON/MUNDO 02/Exercicios/Ex65_media_maior_menor_varios_numeros.py
@@ -6,73 +6,23 @@ Maior valor
 Menor valor
 O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 """
-# media = 0
-# maior = float('-inf')
-# menor = float('inf')
-# num = 0
-# soma = 0
-# num_digitados = 0
-# print('Digite 999 para parar')
-# num = float(input('Digite um número inteiro para a soma: '))
-# while True:
-#     while num != 999:
-#         soma += num
-#         num = float(input('Digite um número inteiro para a soma: '))
-#         if num != 999:
-#             if num > maior:
-#                 maior = num
-#             if num < menor:
-#                 menor = num
-#         num_digitados += 1
-#
-#     continuar = str(input('Deseja mostrar mais números? ')).strip().upper()[0]
-#     if continuar == 'S':
-#         while num != 999:
-#             soma += num
-#             num = float(input('Digite um número inteiro para a soma: '))
-#     else:
-#         break
-# media = soma / num_digitados
-# print('A média entre os valores digitados foi {}'.format(media))
-# print('O maior valor digitado foi {}'.format(maior))
-# print('O menor valor digitado foi {}'.format(menor))
-#
-# print('\nFim do PROGRAMA.')
-
-media = 0
-maior = float('-inf')
-menor = float('inf')
-num = 0
-soma = 0
-num_digitados = 0
-print('Digite 999 para parar')
-num = float(input('Digite um número inteiro para a soma: '))
-while num != 999:
-    soma += num
-    if num > maior:
-        maior = num
-    if num < menor:
-        menor = num
-    num_digitados += 1
-    num = float(input('Digite um número inteiro para a soma: '))
-while True:
-    continuar = str(input('Deseja mostrar mais números? ')).strip().upper()[0]
-    if continuar == 'S':
-        num = float(input('Digite um número inteiro para a soma: '))
-        while num != 999:
-            soma += num
-            if num > maior:
-                maior = num
-            if num < menor:
-                menor = num
-            num_digitados += 1
-            num = float(input('Digite um número inteiro para a soma: '))
+resp = 's'
+soma = quant = media = maior = menor = 0
+while resp in 'Ss':
+    n = int(input('Digite um número: '))
+    soma += n
+    quant += 1
+    if quant == 1:
+        maior = menor = n
     else:
-        break
-if num_digitados != 0:
-    media = soma / num_digitados
-print('A média entre os valores digitados foi {}'.format(media))
-print('O maior valor digitado foi {}'.format(maior))
-print('O menor valor digitado foi {}'.format(menor))
-
-print('\nFim do PROGRAMA.')
+        if n > maior:
+            maior = n
+        if n < menor:
+            menor = n
+    resp = str(input('Deseja continuar? [S/N]')).upper().strip()[0]
+media = soma / quant
+print('FIM DO PROGRAMA')
+print(f'A quantidade de valores digitados foi {quant}')
+print(f'A soma dos valores digitados foi {soma}')
+print(f'A média dos valores é {media}')
+print(f'O maior valor foi {maior} e o menor foi {menor}')
